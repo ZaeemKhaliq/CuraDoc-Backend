5ad6b492e0a5d13df66e4a18c1e4745b012086c3
Muhammad Zaeem Khaliq <khaliqzaeem@hotmail.com>
2022-08-21 14:31:05 +0500

    Updated

--- classes/ErrorHandler.js
+++ classes/ErrorHandler.js
@@ -1,12 +1,13 @@
 class ErrorHandler {
   onCatchResponse(params) {
-    if (!params.res || !params.error) return;
-
     const { res, error, message } = params;
+
+    if (!res || !error) return;
 
     return res.status(500).send({
       message: message || "Some error occurred while processing request.",
-      error: error.message,
+      error,
+      errorMessage: error.message,
     });
   }
 }
--- routes/payment.routes.js
+++ routes/payment.routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const Stripe = require("stripe");
 const router = express.Router();
 
+const ErrorHandler = require("../classes/ErrorHandler");
+
 const stripe = Stripe(
   "sk_test_51JBJRDEKog3E8JbTfKo0A6WfsfcEnSB04wBu2ppNTEc1Ofe1aEylGQ61dsm4rSEvaz1ZfIuyz5k4k1ncn1fWqDK500LZGgsbwB",
   {
@@ -11,12 +13,18 @@ const stripe = Stripe(
 );
 
 router.post("/create-payment-intent", async (req, res) => {
-  const paymentIntent = await stripe.paymentIntents.create({
-    amount: 5000,
-    currency: "usd",
-  });
+  const { appointmentFee } = req.body;
 
-  res.send({ clientSecret: paymentIntent.client_secret });
+  try {
+    const paymentIntent = await stripe.paymentIntents.create({
+      amount: appointmentFee,
+      currency: "usd",
+    });
+
+    return res.status(200).send({ clientSecret: paymentIntent.client_secret });
+  } catch (error) {
+    return ErrorHandler.onCatchResponse({ res, error });
+  }
 });
 
 module.exports = router;
